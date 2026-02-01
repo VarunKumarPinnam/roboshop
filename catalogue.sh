8cd81e711db728dfde2589c10b0f62e246212872
Varun <varunkumar.pinnam@gmail.com>
2026-02-01 11:25:32 +0530

    updated varaiables templates

--- catalogue.sh
+++ catalogue.sh
@@ -41,7 +41,7 @@ id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then 
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
 else 
-    echo  "$Y User already exists, skipping this step $N"
+    echo -e  "$Y User already exists, skipping this step $N"
 fi
 
 mkdir -p /app
