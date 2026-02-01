f3e69b1ccaad76a103494081daba7df5599a5936
Varun <varunkumar.pinnam@gmail.com>
2026-02-01 11:03:57 +0530

    updated varaiables templates

---
+++ catalogue.sh
@@ -0,0 +1,95 @@
+USERID=$(id -u)
+LOGS_DIRECTORY="/var/log/shell-mongodb"
+LOGS_FILE="$LOGS_DIRECTORY/$0.log"
+SHELL_DIR=$PWD
+MONGODB_HOST="mongodb.advidevops.online"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#--ROOT User Check--
+if [ "$USERID" -ne 0 ]; then 
+    echo -e "$R You must run this script with root access $N" 
+    exit 1
+fi
+
+#--Log Setup---
+mkdir -p $LOGS_DIRECTORY
+
+
+validation()
+{
+  if [ $1 -ne 0 ]; then 
+    echo -e "$R $2 Failure $N" | tee -a $LOGS_FILE
+    exit 1
+  else
+    echo -e "$G $2 Success $N" | tee -a $LOGS_FILE
+ fi
+}
+
+dnf module disable nodejs -y 
+validation $? "nodejs module disabling is"
+
+dnf module enable nodejs:20 -y
+validation $? "nodejs module enabling is"
+
+dnf install nodejs -y
+validation $? "nodejs installation is"
+
+id roboshop 
+if [ $? -ne 0 ]; then 
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+else
+    "User already exists, skipping this step"
+fi
+
+mkdir -p /app
+validation $? "creating an app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+validation $? "Download catalogue code"
+
+cd /app 
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+validation $? "removing the existing code"
+
+unzip /tmp/catalogue.zip
+validation $? "unzipping the files"
+
+cd /app
+npm install
+validation $? "installing dependencies" 
+
+cp $SHELL_DIR/catalogue.service /etc/systemd/system/catalogue.service
+validation $? "catalogue service has been updated"
+
+systemctl daemon-reload
+validation $? "system daemon reloaded"
+
+systemctl enable catalogue 
+validation $? "catalogue service enable is"
+
+systemctl start catalogue
+validation $? "catalogue service start is"
+
+cp $SHELL_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-mongosh -y
+validation $? "installing mongodb"
+
+INDEX=$(mongosh --host $MONGODB_HOST  --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then 
+   mongosh --host $MONGODB_HOST </app/db/master-data.js
+   validation $? "loading products"
+else
+    echo "data already loaded skipping this step"
+fi 
+
+systemctl restart catalogue
+
+
+
