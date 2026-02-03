ebfe730acc79db0910c2c568fef7dbd8e17107d3
Varun <varunkumar.pinnam@gmail.com>
2026-02-03 22:09:25 +0530

    updated varaiables templates

--- terminateec2.sh
+++ terminateec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 aws ec2 describe-instances \
-  --filters "Name=tag:Name,Values=redis" \
+  --filters "Name=tag:Name,Values=mongodb,catalogue,user,cart,redis,mysql,rabbitmq,payment,shpping,forntend" \
   --query "Reservations[].Instances[].InstanceId" \
   --output text
